03d5ef680676a00bb00904f9e730621d3df85266
mngill21 <mngill21@uri.edu>
2021-03-28 20:34:29 -0400

    adding facebook authentication

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,14 @@
-print ("Hello World, my name is Maleah and I love Python!")
+# print ("Hello World, my name is Maleah and I love Python!")
 
-userName = input ("Hello! Please enter your name!")
-print ("Hello", userName, "nice to meet you")
+# userName = input ("Hello! Please enter your name!")
+# print ("Hello", userName, "nice to meet you")
+
+userName = input ("Hello! Please enter your facebook username! ")
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "maleah" and userPassword == "Bruins"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
+
